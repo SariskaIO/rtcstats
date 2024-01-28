6ce1f581d705db202346ffcfb1519ab00db22277
brajendrak00068 <brajendrak00068>
2024-01-28 10:22:10 +0530

    fixes

---
+++ dist/dist/events.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.PC_STATE_FAILED = exports.PC_STATE_DISCONNECTED = exports.PC_STATE_CONNECTED = exports.PC_ICE_CON_STATE_CHANGE = exports.PC_CON_STATE_CHANGE = void 0;
+var PC_STATE_CONNECTED = exports.PC_STATE_CONNECTED = 'connected';
+var PC_STATE_DISCONNECTED = exports.PC_STATE_DISCONNECTED = 'disconnected';
+var PC_STATE_FAILED = exports.PC_STATE_FAILED = 'failed';
+var PC_CON_STATE_CHANGE = exports.PC_CON_STATE_CHANGE = 'connectionstatechange';
+var PC_ICE_CON_STATE_CHANGE = exports.PC_ICE_CON_STATE_CHANGE = 'iceconnectionstatechange';
--- dist/dist/obfuscator.js
+++ dist/dist/obfuscator.js
@@ -5,33 +5,75 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = _default;
 var _sdp = _interopRequireDefault(require("sdp"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
-function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; } // obfuscate ip addresses which should not be stored long-term.
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    "default": obj
+  };
+}
+function _slicedToArray(arr, i) {
+  return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
+}
+function _nonIterableRest() {
+  throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+  return arr2;
+}
+function _iterableToArrayLimit(r, l) {
+  var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"];
+  if (null != t) {
+    var e,
+      n,
+      i,
+      u,
+      a = [],
+      f = !0,
+      o = !1;
+    try {
+      if (i = (t = t.call(r)).next, 0 === l) {
+        if (Object(t) !== t) return;
+        f = !1;
+      } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0);
+    } catch (r) {
+      o = !0, n = r;
+    } finally {
+      try {
+        if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return;
+      } finally {
+        if (o) throw n;
+      }
+    }
+    return a;
+  }
+}
+function _arrayWithHoles(arr) {
+  if (Array.isArray(arr)) return arr;
+} // obfuscate ip addresses which should not be stored long-term.
 /**
  * obfuscate ip, keeping address family intact.
  * @param {*} ip
  */
 function maskIP(ip) {
-  // if (ip.indexOf('[') === 0 || ip.indexOf(':') !== -1) {
-  //     // IPv6
-  //     // obfuscate last five bits like Chrome does.
-  //     return `${ip.split(':').slice(0, 3)
-  //         .join(':')}:x:x:x:x:x`;
-  // }
-
-  // const parts = ip.split('.');
-
-  // if (parts.length === 4) {
-  //     parts[3] = 'x';
-
-  //     return parts.join('.');
-  // }
-
+  if (ip.indexOf('[') === 0 || ip.indexOf(':') !== -1) {
+    // IPv6
+    // obfuscate last five bits like Chrome does.
+    return "".concat(ip.split(':').slice(0, 3).join(':'), ":x:x:x:x:x");
+  }
+  var parts = ip.split('.');
+  if (parts.length === 4) {
+    parts[3] = 'x';
+    return parts.join('.');
+  }
   return ip;
 }
 
@@ -41,13 +83,10 @@ function maskIP(ip) {
  * @returns masked IP.
  */
 function obfuscateIP(ip) {
-  // if (ip.indexOf('[') === 0 || ip.indexOf(':') !== -1) {
-
-  //     return 'x:x:x:x:x:x:x:x';
-  // }
-
-  // return 'x.x.x.x';
-  return ip;
+  if (ip.indexOf('[') === 0 || ip.indexOf(':') !== -1) {
+    return 'x:x:x:x:x:x:x:x';
+  }
+  return 'x.x.x.x';
 }
 
 /**
